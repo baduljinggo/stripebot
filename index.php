99adf8ba6ef0874153e5de87562d5beaa033ee66
baduljinggo <109689613+baduljinggo@users.noreply.github.com>
2022-07-21 02:45:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 ////////////////=============[Made with ❤️ by AndryMata]===============////////////////
 
-///https://api.telegram.org/bot<token>/setwebhook?url=<url>
+///https://api.telegram.org/bot5414720154:AAG9aCuzMDt_Sal9Yhz3QnxQSPD-k3YMGMo/setwebhook?url=<url>
 
-$botToken = "1472905848:AAHj-wDq3UJKkCL61VnuK7w-zOPxadnKjlI"; // Enter ur bot token
+$botToken = "5414720154:AAG9aCuzMDt_Sal9Yhz3QnxQSPD-k3YMGMo"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
